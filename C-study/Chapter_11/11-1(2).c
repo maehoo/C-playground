10eafbeaa01c9db6db7cfedb203501492319e76e
MaeHoo <maehoo321@gmail.com>
2025-03-04 21:19:15 +0900

    배열을 이용해 문장 출력

--- C-study/Chapter_11/11-1(2).c
+++ C-study/Chapter_11/11-1(2).c
@@ -4,6 +4,9 @@ int main(void)
 {
    char str[]={'G','o','o','d',' ','t','i','m','e'};
    int arrlen = sizeof(str)/sizeof(char);
+   /*sizeof(str)은 5행의 문자대로 크기가 9로 나오고
+   sizeof(char)은 1이 나온다.
+   그래서 arrlen은 9이다*/
    int i;
    for(i=0; i<arrlen; i++)
     printf("%c", str[i]);
