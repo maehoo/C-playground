6487b2bca98e48ac033fe8319b0d7c8cac1e493b
MaeHoo <maehoo321@gmail.com>
2025-03-08 13:19:51 +0900

    문자열을 저장하는 포인터 배열

---
+++ C-study/Chapter_13/-StringArray.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char * strarr[3]={"simple", "string", "array"};
+    printf("%s \n", strarr[0]);
+    printf("%s \n", strarr[1]);
+    printf("%s \n", strarr[2]);
+    return 0;
+}
