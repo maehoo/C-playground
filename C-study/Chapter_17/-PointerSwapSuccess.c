d60126b1ffffe4182d710fc0212bcc70e9cd49ea
MaeHoo <maehoo321@gmail.com>
2025-03-12 12:41:09 +0900

    더블 포인터를 이용해 값 뒤바꾸기 !중요!

--- C-study/Chapter_17/-PointerSwapSuccess.c
+++ C-study/Chapter_17/-PointerSwapSuccess.c
@@ -3,7 +3,11 @@
 void swapintptr(int **dp1, int **dp2)
 {
     int * temp = *dp1;
-    *dp1 = *dp2;
+    *dp1 = *dp2;//값을 바꾸는 거임 
+    //num1 = num2; 이 구문도 num1 안에 있는 값을 num2안에 있는 값으로 대체한다.
+    //라는 뜻이다. 똑같이 *dp1 = *dp2 하게 되면  ptr1 =ptr2가 되면서 
+    //ptr1 이 가리키는 &num1을 ptr2가 가리키는 &num2로 바꾼다. 라는 뜻이다.!!
+    //중요중요! 
     *dp2 = temp;
 }
 int main(void)
