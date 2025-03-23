1450e56d747c20b0af4a3077462d87390d2e4257
MaeHoo <maehoo321@gmail.com>
2025-03-23 12:47:10 +0900

    getchar, putchar 활용 - 2

--- C-study/Chapter_21/21-1(1.1).c
+++ C-study/Chapter_21/21-1(1.1).c
@@ -19,8 +19,10 @@ int main(void)
     ch=convcase(ch);
     if(ch==-1)
     {
-        puts("범위를 벗어난 입력입니다");
-        return -1;
+        puts("범위를 벗어난 입력입니다");//printf()	문자열 + 변수까지 출력 가능
+                                   //puts() 문자열만 출력, 자동으로 줄바꿈
+        return -1; //return 0; 프로그램 정상 종료
+                   //return -1; 프로그램 오류 종료
     }
     putchar(ch);
     return 0; 
