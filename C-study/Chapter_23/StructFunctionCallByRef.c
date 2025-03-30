dd9cab99f420c5505d65eb4ba1ccbb25530c8a9a
MaeHoo <maehoo321@gmail.com>
2025-03-30 15:00:48 +0900

    함수 인자 전달, return 변환 포인터 구조체 변수

---
+++ C-study/Chapter_23/StructFunctionCallByRef.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct point
+{
+    int xpos;
+    int ypos;
+} Point;
+
+void orgsymtrans(Point *ptr)
+{
+    ptr->xpos = (ptr->xpos) * (-1);
+    ptr->ypos = (ptr->ypos) * (-1);
+}
+
+void showposition(Point pos)
+{
+    printf("[%d %d] \n", pos.xpos, pos.ypos);
+}
+
+int main(void)
+{
+    Point pos = {7,-5};
+    orgsymtrans(&pos);
+    showposition(pos);
+    orgsymtrans(&pos);
+    showposition(pos);
+    return 0;
+}
