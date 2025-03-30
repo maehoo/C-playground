d191d676290c1367a2d29a9ff7ae8e630d881386
MaeHoo <maehoo321@gmail.com>
2025-03-30 14:55:05 +0900

    함수 인자 전달, return 변환되는 배열 구조체 변수

---
+++ C-study/Chapter_23/StructMemArrCopy.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+typedef struct person
+{
+    char name[20];
+    char phonenum[20];
+    int age;
+} Person;
+
+void showpersoninfo(Person man)
+{
+    printf("name: %s\n",man.name);
+    printf("phone: %s\n",man.phonenum);
+    printf("age: %d\n",man.age);
+}
+
+Person readpersoninfo(void)
+{
+    Person man;
+    printf("name? "); scanf("%s",man.name);
+    printf("phone? "); scanf("%s",man.phonenum);
+    printf("age? "); scanf("%d",&man.age);
+    return man;
+}
+
+int main(void)
+{
+    Person man=readpersoninfo();
+    showpersoninfo(man);
+    return 0;
+}
