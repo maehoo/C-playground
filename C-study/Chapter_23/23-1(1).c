63266da9be9d70ad7abb3c96e656c2b3312d4f4f
MaeHoo <maehoo321@gmail.com>
2025-03-30 16:34:53 +0900

    구조체 값 뒤바꾸기

---
+++ C-study/Chapter_23/23-1(1).c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct point
+{
+    int xpos;
+    int ypos;
+} Point;
+
+void swappoint(Point * ptr1, Point * ptr2)
+{
+    Point tpos={ptr2->xpos,ptr2->ypos};
+    ptr2->xpos = ptr1->xpos; ptr2->ypos = ptr1->ypos;
+    ptr1->xpos = tpos.xpos; ptr1->ypos = tpos.ypos;
+}
+
+int main(void)
+{
+    Point pos1={2,4};
+    Point pos2={5,7};
+    printf("pos1 [%d %d]  pos2 [%d %d] \n",pos1.xpos, pos1.ypos, pos2.xpos, pos2.ypos);
+    swappoint(&pos1,&pos2);
+    printf("pos1 [%d %d]  pos2 [%d %d] \n",pos1.xpos, pos1.ypos, pos2.xpos, pos2.ypos);
+    return 0;
+}
