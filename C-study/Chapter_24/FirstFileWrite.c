30f94558f191308c1c513d3ffc5c761fb18e09a5
MaeHoo <maehoo321@gmail.com>
2025-04-01 16:21:06 +0900

    파일에 데이터 입력

---
+++ C-study/Chapter_24/FirstFileWrite.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    FILE * fp=fopen("data.txt","wt");
+    if(fp==NULL){
+        puts("파일오픈실패");
+        return -1;
+    }
+
+    fputc('A',fp);
+    fputc('B',fp);
+    fputc('C',fp);
+    fputc('D',fp);
+    fclose(fp);
+    return 0;
+}
